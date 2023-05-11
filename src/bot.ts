cd1c26e0a8f8daef41589c8378c0a864c4ed7085
Matt Cavanagh <maelstrome26@gmail.com>
2023-05-11 22:05:41 +0100

    feat: bring in dotenv

--- src/bot.ts
+++ src/bot.ts
@@ -1,10 +1,14 @@
 import { Client } from "discord.js";
 import ready from "./listeners/ready";
 import interactionCreate from "./events/InteractionCreate";
+import * as dotenv from 'dotenv'
+
+// Grab configs
+dotenv.config()
 
 console.log("Bot is starting...");
 
-const token = "MTEwNjMxODg4OTMxNjUyODE3OA.GCZ18R.WJLOW-YfBwDdJnpKlRW1L0PlwAwwTtgDELoF0s"
+const token = process.env.TOKEN
 
 const client = new Client({
     intents: []
