f42f9659e807395755da5c85a9590761a9074e57
Matt Cavanagh <maelstrome26@gmail.com>
2023-09-26 00:36:48 +0100

    feat: Upgraded Albion registration service to commit users to database

--- src/config/albion.app.config.ts
+++ src/config/albion.app.config.ts
@@ -1,3 +1,20 @@
+export interface AlbionRoleMapInterface {
+  [key: string]: {
+    discordRoleId: string;
+  };
+}
+// const roleMapDevelopment = {
+//   '@ALB/Enlisted': {
+//     discordRoleId: '1155987100928323594',
+//   },
+// };
+//
+// const roleMapProduction = {
+//   '@ALB/Enlisted': {
+//     discordRoleId: '1155987035472023702',
+//   },
+// };
+
 export default () => ({
   guildGameId: 'btPZRoLvTUqLC7URnDRgSQ',
 });
