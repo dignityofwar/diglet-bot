4f56b55d6d30fefc9a84c2e6607747e1b02a6bc9
Matt Cavanagh <maelstrome26@gmail.com>
2023-10-19 03:07:51 +0100

    fix: Increased buffer size, done the math it should fit

--- src/albion/services/albion.reports.service.ts
+++ src/albion/services/albion.reports.service.ts
@@ -204,7 +204,7 @@ export class AlbionReportsService {
 
       messageBuffer += `${initiate}\n`;
 
-      if (count % 5 === 0 || count === membersReportArray.length) {
+      if (count % 10 === 0 || count === membersReportArray.length) {
         messagesInBuffer.push(messageBuffer);
         messageBuffer = '';
       }
