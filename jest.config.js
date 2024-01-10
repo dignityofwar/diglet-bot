eba968b0d25a34673d81b08425e1737cd91237fd
Matt Cavanagh <maelstrome26@gmail.com>
2024-01-10 18:45:55 +0000

    feat(#0): Added / fixed jest coverage reports, baked it into the build

--- jest.config.js
+++ jest.config.js
@@ -10,10 +10,16 @@ module.exports = {
   ],
   rootDir: 'src',
   testRegex: '.*\\.spec\\.ts$',
+  collectCoverage: true,
   collectCoverageFrom: [
-    '**/*.(t|j)s',
+    '**/*.{ts,tsx}', // Include only TypeScript and TSX files
+    '**/*.spec.ts', // Explicitly exclude test files
+    '**/*index.ts', // Exclude index files if they just re-export, as an example
+    '!database/**', // Exclude database folder, filled with migrations that's pointless to test
+    '!config/**', // Exclude config folder, mostly pointless to test
+    '!**/*.module.ts', // Exclude module files, really hard to test with not much value
   ],
   coverageDirectory: 'coverage',
+  coverageReporters: ['json-summary', 'text'],
   setupFilesAfterEnv: ['./jest-preload.js'],
-  coverageReporters: ['json-summary'],
 };
