bf95bb118359259e0beaae47dbc0debf9388feb2
Matt Cavanagh <maelstrome26@gmail.com>
2024-05-25 21:58:50 +0100

    feat #101: Added command that scans the activity data and flushes out purged or leaving members. (#102)
    * feat #101: Added command that scans the activity data and flushes out purged or leaving members.
    
    * feat #101: Added number of remaining records. Fixed backup script
    
    * feat #101: Added activity service tests
    
    * feat #101: Added activity cron service tests

--- src/general/general.module.ts
+++ src/general/general.module.ts
@@ -11,16 +11,21 @@ import { MessageEvents } from './events/message.events';
 import { DatabaseModule } from '../database/database.module';
 import { DatabaseService } from '../database/services/database.service';
 import { VoiceStateEvents } from './events/voice.state.events';
+import { ActivityScanCommand } from './commands/activity.scan.command';
+import { ActivityService } from './services/activity.service';
+import { ActivityCronService } from './services/activity.cron.service';
 
 @Module({
   imports: [DiscordModule.forFeature(), ConfigModule, DatabaseModule],
   providers: [
     // Services
+    ActivityService,
     DatabaseService,
     DiscordService,
     PurgeService,
 
     // Commands
+    ActivityScanCommand,
     PingCommand,
     PurgeCandidatesCommand,
     ThanosSnapCommand,
@@ -28,6 +33,9 @@ import { VoiceStateEvents } from './events/voice.state.events';
     // Events
     MessageEvents,
     VoiceStateEvents,
+
+    // Cron Services
+    ActivityCronService,
   ],
 })
 export class GeneralModule {}
