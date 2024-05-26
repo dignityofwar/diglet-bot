297f732daf6ea1c3489ea3f3600dfe348d8ab5ca
Matt Cavanagh <maelstrome26@gmail.com>
2024-05-26 15:14:59 +0100

    fix(quality): Simplify leavers logic further, if they have not left either server or guild, nothing to do

--- src/albion/services/albion.scanning.service.ts
+++ src/albion/services/albion.scanning.service.ts
@@ -201,57 +201,60 @@ export class AlbionScanningService {
         leftServer = true;
       }
 
-      if (leftGuild || leftServer) {
-        // Construct the appropriate message
-        if (leftGuild && leftServer) {
-          leavers.push(`- ${emoji} 💁 Character / Player ${character.Name} has left **both** the DIG server and the Guild. They are dead to us now 💅`);
-        }
-        else if (leftGuild && discordMember) {
-          leavers.push(`- ${emoji} 👋 <@${discordMember.id}>'s character **${character.Name}** has left the Guild but remains on the Discord server. Their roles and registration status have been stripped.`);
-        }
-        else if (leftServer) {
-          leavers.push(`- ${emoji} ‼️🫥️ Discord member for Character **${character.Name}** has left the DIG Discord server. Their registration status has been removed. **They require booting from the Guild!**`);
-          actionRequired = true;
-        }
+      // If they remain, nothing left to do
+      if (!leftGuild && !leftServer) {
+        continue;
+      }
 
-        // If dry run, nothing left to do.
-        if (dryRun) {
+      // Construct the appropriate message
+      if (leftGuild && leftServer) {
+        leavers.push(`- ${emoji} 💁 Character / Player ${character.Name} has left **both** the DIG server and the Guild. They are dead to us now 💅`);
+      }
+      else if (leftGuild && discordMember) {
+        leavers.push(`- ${emoji} 👋 <@${discordMember.id}>'s character **${character.Name}** has left the Guild but remains on the Discord server. Their roles and registration status have been stripped.`);
+      }
+      else if (leftServer) {
+        leavers.push(`- ${emoji} ‼️🫥️ Discord member for Character **${character.Name}** has left the DIG Discord server. Their registration status has been removed. **They require booting from the Guild!**`);
+        actionRequired = true;
+      }
+
+      // If dry run, nothing left to do.
+      if (dryRun) {
+        continue;
+      }
+
+      // Delete their registration record
+      try {
+        await this.albionRegistrationsRepository.removeAndFlush(member);
+      }
+      catch (err) {
+        await message.channel.send(`ERROR: Unable to remove Albion Character "${character.Name}" (${character.Id}) from registration database! Pinging <@${this.config.get('discord.devUserId')}>!`);
+      }
+
+      // If Discord member does not exist, there are no discord actions to take.
+      if (!discordMember) {
+        continue;
+      }
+
+      // Strip their roles if they still remain on the server
+      // Remove all roles from the user
+      for (const roleMap of Object.values(roleMaps)) {
+        // If role is not for the correct server, skip it
+        if (roleMap.server !== server) {
           continue;
         }
 
-        // Delete their registration record
-        try {
-          await this.albionRegistrationsRepository.removeAndFlush(member);
-        }
-        catch (err) {
-          await message.channel.send(`ERROR: Unable to remove Albion Character "${character.Name}" (${character.Id}) from registration database! Pinging <@${this.config.get('discord.devUserId')}>!`);
-        }
+        // Force fetch the role, so we get a proper list of updated members
+        const role = message.guild.roles.cache.get(roleMap.discordRoleId);
+        // Check if the user still has the role
+        const hasRole = role.members.has(discordMember.id);
 
-        // If Discord member does not exist, there are no discord actions to take.
-        if (!discordMember) {
-          continue;
-        }
-
-        // Strip their roles if they still remain on the server
-        // Remove all roles from the user
-        for (const roleMap of Object.values(roleMaps)) {
-          // If role is not for the correct server, skip it
-          if (roleMap.server !== server) {
-            continue;
+        if (hasRole) {
+          try {
+            await discordMember.roles.remove(roleMap.discordRoleId);
           }
-
-          // Force fetch the role, so we get a proper list of updated members
-          const role = message.guild.roles.cache.get(roleMap.discordRoleId);
-          // Check if the user still has the role
-          const hasRole = role.members.has(discordMember.id);
-
-          if (hasRole) {
-            try {
-              await discordMember.roles.remove(roleMap.discordRoleId);
-            }
-            catch (err) {
-              await message.channel.send(`ERROR: Unable to remove role "${role.name}" from ${character.Name} (${character.Id}). Err: "${err.message}". Pinging <@${this.config.get('discord.devUserId')}>!`);
-            }
+          catch (err) {
+            await message.channel.send(`ERROR: Unable to remove role "${role.name}" from ${character.Name} (${character.Id}). Err: "${err.message}". Pinging <@${this.config.get('discord.devUserId')}>!`);
           }
         }
       }
