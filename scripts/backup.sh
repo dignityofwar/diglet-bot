e034fc9ca7c435edf106305df3ac3c7e4f7622ff
Matt Cavanagh <maelstrome26@gmail.com>
2024-05-26 14:11:11 +0100

    fix: Realised that I had a duplicate of the database scripts

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 mariadb-dump -h 10.0.5.2 -P 3307 -u root digletbot -p --databases digletbot > ~/dumps/diglet-bot/fulldump.sql
 echo "Backup complete, file located at ~/dumps/diglet-bot/fulldump.sql"
