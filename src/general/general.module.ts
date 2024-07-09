37f7af072c0fc94534922397b2e9f667347eb661
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2024-07-09 17:14:08 +0200

    feat: #67: Removed purge candidates command, this can be handled with a purge dry run now the report is in there.

--- src/general/general.module.ts
+++ src/general/general.module.ts
@@ -3,7 +3,6 @@ import { Module } from '@nestjs/common';
 
 import { PingCommand } from './commands/ping.command';
 import { ConfigModule } from '../config/config.module';
-import { PurgeCandidatesCommand } from './commands/purge.candidates.command';
 import { PurgeService } from './services/purge.service';
 import { ThanosSnapCommand } from './commands/thanos.snap.command';
 import { DiscordService } from '../discord/discord.service';
@@ -28,7 +27,6 @@ import { ActivityService } from './services/activity.service';
     // Commands
     ActivityScanCommand,
     PingCommand,
-    PurgeCandidatesCommand,
     ThanosSnapCommand,
 
     // Events
